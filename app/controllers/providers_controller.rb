0887a113df74f6809a7083222dc08fe57debc449
Sean Connolly <sean.connolly@northwestern.edu>
2024-03-14 14:34:22 +0000

    still working on new user

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,20 +2,20 @@ class ProvidersController < ApplicationController
   def new
   end
 
-  def index
-    render 'index'
-  end
-
   def create
-    @provider = provider_id.new
-    @provider["first_name"] = params["first_name"]
-    @provider["last_name"] = params["last_name"]
-    @provider["email"] = params["email"]
-    @provider["NPI"] = params["NPI"]
-    @provider["password"] = BCrypt::Password.create(params["password"])
-    @provider.save
-    flash[:notice] = "Thanks for signing up. Now login."
-    redirect_to "/login"
+    if Provider.find_by({ "email" => params["email"] }) == nil
+     @provider = Provider.new
+     @provider["first_name"] = params["first_name"]
+     @provider["last_name"] = params["last_name"]
+     @provider["email"] = params["email"]
+     @provider["NPI"] = params["NPI"]
+     @provider["password"] = BCrypt::Password.create(params["password"])
+     @provider.save
+     redirect_to "/login"
+     flash[:notice] = "Thanks for signing up. Now, login."
+    else
+      flash["notice"] = "Email taken."
+      redirect_to "/providers/new"
+
   end
 end
-
--- db/migrate/20240312224559_create_providers.rb
+++ db/migrate/20240312224559_create_providers.rb
@@ -1,6 +1,11 @@
-class CreateProviders < ActiveRecord::Migration[7.1]
+class CreateProviders < ActiveRecord::Migration[7.0]
   def change
     create_table :providers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :npi
+      t.string :password
 
       t.timestamps
     end
